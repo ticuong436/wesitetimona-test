dd7261f6d7b31ef76f3f9297b6116ec9ee1a7dd7
cuong <ticuong436@gmail.com>
2022-02-24 19:48:44 +0700

    done

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from "@angular/material/list"
+// import { HomePageComponent } from './components/home-page/home-page.component';
+import { HomePagesComponent } from './components/home-pages/home-pages.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+    
+          HomePagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
